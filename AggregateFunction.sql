00ef87ee7766ebf74280084ca285046dd93fb184
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2022-03-04 14:45:37 -0500

    Add files via upload

---
+++ AggregateFunction.sql
@@ -0,0 +1,6 @@
+SELECT Min(replacement_cost), MAX(replacement_cost),COUNT(replacement_cost),SUM(replacement_cost),ROUND(AVG(replacement_cost),3) FROM film
+--WHERE first_name ILIKE 'j%' AND last_name ILIKE 'j%'
+--ORDER BY amount DESC
+--WHERE first_name LIKE '_%er%' AND last_name NOT LIKE '%B'
+--ORDER BY last_name DESC
+LIMIT 5;

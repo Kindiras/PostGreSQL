1e1fbd4b2dd94e19639a3fcdceafc3050dc5f7c9
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2022-03-08 16:28:31 -0500

    Update GroupBy.sql

--- GroupBy.sql
+++ GroupBy.sql
@@ -1,4 +1,4 @@
-SELECT customer_id,staff_id, SUM(amount) AS tAmount FROM payment
+SELECT staff_id,customer_id, SUM(amount) AS tAmount FROM payment
 WHERE customer_id !=599
 GROUP BY staff_id, customer_id
 ORDER BY customer_id DESC;

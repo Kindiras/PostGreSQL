c524afe12dd27e200b743bd757e1d5b695a656b4
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2022-03-30 17:52:01 -0400

    Add files via upload

---
+++ Insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO account(username,password,email,created_on)
+VALUES
+('Jose','check','j@test.com',CURRENT_TIMESTAMP)
---
+++ Insert2.sql
@@ -0,0 +1,3 @@
+INSERT INTO account_job(user_id,job_id,hire_date)
+VALUES
+(1,1,CURRENT_TIMESTAMP)--user_id and job_id should match with other table
